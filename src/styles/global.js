b3850137a5f20378481426e016ea2d3d2e4e5062
Dam450 <68165752+dam450@users.noreply.github.com>
2023-03-22 15:57:12 -0300

    :tada: First commit

---
+++ src/styles/global.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    background: grey;
+    color: white;
+    min-height: 100vh;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter 0.2;
+  }
+
+  button:hover, a:hover {
+    filter: brightness(0.9);
+  }
+`;
