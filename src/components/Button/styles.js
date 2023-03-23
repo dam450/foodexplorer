20e22d4f25df78289e9114a6b13cb62adc043e4e
Dam450 <68165752+dam450@users.noreply.github.com>
2023-03-23 18:18:28 -0300

    create Button component

---
+++ src/components/Button/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  background-color: #750310;
+  color: #ffffff;
+
+  border: none;
+  border-radius: 5px;
+
+  /* flex-direction: row; */
+  justify-content: center;
+  /* align-items: center; */
+  padding: 12px 32px;
+  gap: 8px;
+
+  /* height: 48px; */
+
+  /* Poppins/100-medium */
+
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 24px;
+  /* identical to box height, or 171% */
+
+  /* display: flex;
+  align-items: center;
+  text-align: center; */
+
+  /* Light/Light 100 */
+`;
