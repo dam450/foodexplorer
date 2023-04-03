78893c11e0a678423984f603cd658a5cb4a7e1d3
Dam450 <68165752+dam450@users.noreply.github.com>
2023-04-03 19:37:29 -0300

    feat: register user

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,13 +1,39 @@
-import { Container } from './styles';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import logo from '../../assets/logo.svg';
+import { Container } from './styles';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 
+import logo from '../../assets/logo.svg';
+
+import { api } from '../../services/api';
+
 export function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
   function handleSignUp(event) {
     event.preventDefault();
-    alert('Sign Up');
+
+    if (!name || !email || !password) alert('Por favor, preencha todos os valores!');
+
+    api
+      .post('/users', { name, email, password })
+      .then(() => {
+        alert('Usuário cadastrado com sucesso');
+        navigate('/');
+      })
+      .catch((err) => {
+        if (err.response) {
+          alert(err.response.data.message);
+        } else {
+          alert('Não foi possível cadastrar o usuário');
+        }
+      });
   }
 
   return (
@@ -21,13 +47,21 @@ export function SignUp() {
         <h2 className="formTitle">Crie sua conta</h2>
         <form>
           <label htmlFor="email">Seu nome</label>
-          <Input id="name" type="name" placeholder="Exemplo: Maria da Silva" />
+          <Input
+            id="name"
+            type="name"
+            placeholder="Exemplo: Maria da Silva"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
 
           <label htmlFor="email">Email</label>
           <Input
             id="email"
             type="email"
             placeholder="Exemplo: exemplo@exemplo.com.br"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
 
           <label htmlFor="password">Senha</label>
@@ -35,6 +69,8 @@ export function SignUp() {
             id="password"
             type="password"
             placeholder="No mínimo 6 caracteres"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
 
           <Button type="submit" onClick={handleSignUp}>
