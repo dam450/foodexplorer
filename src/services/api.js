911b0a239e534c8a1b54472763844b2933e8ca90
Dam450 <68165752+dam450@users.noreply.github.com>
2023-04-12 15:02:53 -0300

    Update api info

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-const port = '3333';
+const proto = 'http';
 const host = 'localhost';
+const port = '3333';
 
 /**
  * @exports {AxiosInstance}
  */
 export const api = axios.create({
-  baseURL: `http://${host}:${port}`,
+  baseURL: `${proto}://${host}:${port}`,
 });
