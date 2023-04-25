73d44209d9a6d692bfa05a5bfa5e69db0fbe6ee1
Dam450 <68165752+dam450@users.noreply.github.com>
2023-04-24 23:10:48 -0300

    create tag component

---
+++ src/components/Tag/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.span`
+  /* justify-content: center; */
+  /* align-items: center; */
+  /* gap: 8px; */
+  padding: 0.4rem 0.8rem;
+
+  width: auto;
+  height: 3.2rem;
+  margin: 1.2rem;
+
+  white-space: nowrap;
+
+  background: #192227;
+  border-radius: 0.5rem;
+
+  font-family: 'Poppins';
+  font-weight: 500;
+  font-size: 1.4rem;
+  line-height: 2.4rem;
+
+  color: #ffffff;
+`;
