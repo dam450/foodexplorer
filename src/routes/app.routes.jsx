15d5416989ff32a76a433214f5a4835f2ed10019
Dam450 <68165752+dam450@users.noreply.github.com>
2023-04-24 23:30:26 -0300

    update routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,14 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, redirect, Navigate } from 'react-router-dom';
 
 import { Home } from '../pages/Home';
 import { NewDish } from '../pages/NewDish';
+import { Dish } from '../pages/Dish';
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route index element={<Home />} />
+      {/* <Route index element={<Home />} /> */}
+      <Route path="/home" element={<Home />} />
       <Route path="/new" element={<NewDish />} />
+      <Route path="/dish/:id" element={<Dish />} />
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<Navigate to={'/'} replace />} />
     </Routes>
   );
 }
